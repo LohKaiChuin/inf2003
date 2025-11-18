1b4e7bd2bdbe07a8041e57725f1d94a70b7dc69d
LohKaiChuin <2401764@sit.singaporetech.edu.sg>
2025-11-18 14:40:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Database</title>
+</head>
+
+<body>
+    <main>
+        <!-- Main Section -->
+        <section class="main">
+            <h1>DatabaseProject
+        </section>
+    </main>
+</body>
+</html>
