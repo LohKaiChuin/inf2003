9ed5037506bc58dab928b142326b23d568544e7e
LohKaiChuin <2401764@sit.singaporetech.edu.sg>
2025-11-30 04:37:42 +0800

    Update navbar.inc.php

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -1,9 +1,8 @@
 <nav>
     <a href="logout.php" class="btn btn-outline-primary">Logout</a>
-    <a href="poi.php" class="btn btn-outline-primary">POI</a>
     <a href="manage_users.php" class="btn btn-outline-primary">Manage Users</a>
 
-    <a href="index.php" class="btn btn-outline-primary">
+    <a href="admin_dashboard.php" class="btn btn-outline-primary">
     <svg width="16" height="16" fill="currentColor" class="me-2" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
     </svg>Back to Dashboard</a>
