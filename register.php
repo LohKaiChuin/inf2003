4f19b8127933d8b624516f5b8102bdbc45149afc
Proz2121 <jasonpan65@gmail.com>
2025-11-29 20:35:03 +0800

    same improvement

--- register.php
+++ register.php
@@ -155,11 +155,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             justify-content: center;
             align-items: center;
             padding: 20px;
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
             overflow-x: hidden;
             overflow-y: auto;
         }
